62f28b80833e3d359e02438d172dd10d4b64e657
LokeshVishwakarma <lokeshvishwakarma2611@gmail.com>
2022-10-19 16:57:31 +0530

    ready for first deployment

--- config/index.js
+++ config/index.js
@@ -1,7 +1,10 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
 module.exports = {
   socketConfig: {
     cors: {
-      origin: "*",
+      origin: process.env.FRONTEND_URL || "http://localhost:3000",
       credentials: true,
       methods: ["GET", "POST"],
     },
